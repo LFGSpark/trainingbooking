d19c7855679c4ecc4933237a56e0468f5d82d856
luisfernando <luisfernando.gomez@sparktechs.com>
2021-11-02 16:08:15 -0700

    se anadio el rest api post

--- send-data.php
+++ send-data.php
@@ -14,7 +14,7 @@
 
     $string = http_build_query($postData);
     $url = 'https://personal-hezqtnbk.outsystemscloud.com/GymBooking/rest/Entrenamientos/CrearCita';
-    $jsonResponse = rest_call("POST",$url, $string, 'application/x-www-form-urlencoded');
+    $jsonResponse = rest_call("POST", $url, $string, 'application/x-www-form-urlencoded');
     $response = json_decode($jsonResponse);
 
 ?>
