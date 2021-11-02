b3e72aa87c8f7e5a4dcef88cc5411389996325f5
luisfernando <luisfernando.gomez@sparktechs.com>
2021-11-02 16:05:21 -0700

    se anadio el rest api post

---
+++ send-data.php
@@ -0,0 +1,20 @@
+<?php
+
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $tipo = $_POST['tipo'];
+    $capacidad = $_POST['capacidad'];
+
+    $postData = array(
+        'fecha'=> $fecha,
+        'hora'=> $hora,
+        'tipo'=> $tipo,
+        'capacidad'=> $capacidad
+        );
+
+    $string = http_build_query($postData);
+    $url = 'https://personal-hezqtnbk.outsystemscloud.com/GymBooking/rest/Entrenamientos/CrearCita';
+    $jsonResponse = rest_call("POST",$url, $string, 'application/x-www-form-urlencoded');
+    $response = json_decode($jsonResponse);
+
+?>
