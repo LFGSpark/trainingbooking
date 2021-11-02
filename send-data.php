4dab49e892bd52843033f517ac24a04a0de2a793
luisfernando <luisfernando.gomez@sparktechs.com>
2021-11-02 16:28:11 -0700

    se anadio el rest api post

--- send-data.php
+++ send-data.php
@@ -5,17 +5,46 @@
     $tipo = $_POST['tipo'];
     $capacidad = $_POST['capacidad'];
 
-    $postData = array(
+    $info = array(
         'fecha'=> $fecha,
         'hora'=> $hora,
         'tipo'=> $tipo,
         'capacidad'=> $capacidad
         );
 
-    $string = http_build_query($postData);
-    $url = 'https://personal-hezqtnbk.outsystemscloud.com/GymBooking/rest/Entrenamientos/CrearCita';
-    $jsonResponse = rest_call("POST", $url, $string, 'application/x-www-form-urlencoded');
-    $response = json_decode($jsonResponse);
+        $url = "https://personal-hezqtnbk.outsystemscloud.com/GymBooking/rest/Entrenamientos/CrearCita";
+        
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array(
+            "Accept: application/json",
+            "Content-Type: application/json",
+         );
+        
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        
+        $data = <<<DATA
+        {
+          "fecha": "2021-11-05",
+          "hora": "22:00:00",
+          "tipo": bike,
+          "capacidad": 1
+        }
+        DATA;
+
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        var_dump($resp);
 
     header("Location: index.php");
 
