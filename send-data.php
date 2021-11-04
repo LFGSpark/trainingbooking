b03ff3cda64b6c07c1feb73c9d29a668c1819de2
luisfernando <luisfernando.gomez@sparktechs.com>
2021-11-04 10:42:38 -0700

    modificacion api rest post consume

--- send-data.php
+++ send-data.php
@@ -1,5 +1,6 @@
 <?php
 
+    //inicializacion de variables
     $fecha = $hora = $tipo = $capacidad = $nombre = $fields_string = "";
 
     $fecha = $_POST['fecha'];
@@ -8,12 +9,16 @@
     $capacidad = $_POST['capacidad'];
     $nombre = $_POST['nombre'];
 
+    //formato de hora correcto para outsystems
     $horaCF = $hora . ":00";
+
+    //se remplazan espacios en variable nombre
     $nombre = str_replace(' ','-', $nombre);
 
-
+    //url del POST
     $url = "https://personal-hezqtnbk.outsystemscloud.com/GymBooking/rest/Entrenamientos/CrearCita?fecha=".$fecha."&hora=".$horaCF."&tipo=".$tipo."&capacidad=".$capacidad."&nombre=".$nombre;
     
+    //arreglo de variables (sin uso)
     $fields = array(
         'fecha' => urlencode($_POST['fecha']),
         'hora' => urlencode($_POST['hora']),
@@ -24,19 +29,19 @@
     foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
     rtrim($fields_string, '&');
 
-    //open connection
+    //inicia curl
     $ch = curl_init();
 
-    //set the url, number of POST vars, POST data
+    //config de curl
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, true);
     //curl_setopt($ch, CURLOPT_POST, count($fields));
     //curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 
-    //execute post
+    //consume el api rest post con el url proveido
     $result = curl_exec($ch);
 
-    //close connection
+    //cierra curl
     curl_close($ch);
     
     header("Location: index.php");
